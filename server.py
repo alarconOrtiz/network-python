5f60a6ede9dc731708e613a6cca9c510f678c8d4
alarcon <miguel.alarcon.ortiz@gmail.com>
2017-03-26 19:20:03 +0200

    working on import troubles

--- server.py
+++ server.py
@@ -2,14 +2,23 @@ import socket
 
 class Server( object ):
     """docstring for ."""
+    #declararion of class variables
+    _status_running_server = 0 #false
 
     def __init__( self, port, status ):
-        self.port   = port
-        self.status = status
+        self.port      = port
+        self.status    = status
+        self.socket_fd = socket.Socket()
 
    #methods to work with server.
     def Run_sever( self ):
-        print 'Server running'
+        _status_running_server = 1
+        socket_fd.bind( 'localhost', self.port )
+        socket_fd.listen( 5 )
+        while _status_running_server:
+            socket_c, (host_c, puerto_c) = socketfd.accept()
+            print 'recived packet from client.'
+        socketfd.close()
 
     def Stop_sever( self ):
-        print 'Server stopped'
+        _status_running_server = 0
