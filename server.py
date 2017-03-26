cf017e2b81f2447dff6926f255faa3de3b78b90d
alarcon <miguel.alarcon.ortiz@gmail.com>
2017-03-26 23:04:38 +0200

    update example on github

--- server.py
+++ server.py
@@ -8,16 +8,18 @@ class Server( object ):
     def __init__( self, port, status ):
         self.port      = port
         self.status    = status
-        self.socket_fd = socket.Socket()
+        self.socket_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4 & TCP
 
    #methods to work with server.
     def Run_sever( self ):
         _status_running_server = 1
-        socket_fd.bind( 'localhost', self.port )
-        socket_fd.listen( 5 )
+        self.socket_fd.bind( 'localhost', self.port )
+        self.socket_fd.listen( 5 )
+        print 'ready to listen'
         while _status_running_server:
-            socket_c, (host_c, puerto_c) = socketfd.accept()
+            socket_c, (host_c, puerto_c) = self.socketfd.accept()
             print 'recived packet from client.'
+
         socketfd.close()
 
     def Stop_sever( self ):
