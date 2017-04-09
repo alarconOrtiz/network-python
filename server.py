2b0a6c926bbcf9fc3a4b8dd3804b5d5514a1c400
alarcon <miguel.alarcon.ortiz@gmail.com>
2017-04-09 18:09:39 +0200

    added ID=0 to server.

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ class Server( object ):
     """this class contains a server tcp where its port is given by a constructor"""
     #declararion of class variables
 
-
     def __init__( self, port ):
         self.port      = port
         self.__status_running_server = 0
         self.socket_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4 & TCP
+        self.id = 0
 
    #methods to work with server.
     def Run_sever( self ):
