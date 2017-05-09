a7b42303254e8e72bdc5d7ddaf378651177b515f
alarcon <miguel.alarcon.ortiz@gmail.com>
2017-05-09 18:40:21 +0200

    finish the message class to handle data

--- message.py
+++ message.py
@@ -3,21 +3,26 @@ class Message ( Frame ):
     """
     this class works like a basic structure to compose the format of the message .
     Message has several records:
-        id = identifier per each client, forbiden 0 which is a Server ID
+        id      = identifier per each client, forbiden 0 which is a Server ID
         command = it is a identifier of frame.
         payload = information of command.
-        ??len   = length of frame recieved.
+        len     = length of frame recieved.
     """
 
-    def __init__():
-        self.arg     = arg
-        self.id      = 0
-        self.command = 0
-        self.payload = 0
+    def __init__( self ):
+        self.mId_src  = 0
+        self.mId_dst  = 0
+        self.mLen     = 0
+        self.mCommand = 0
+        self.mPayload = 0
 
-    def Create_message(id,command,payload):
+    def Create_message( self, id_src, id_dst, command, payload):
+        data = [len, id_dst, id_src, command, payload]
+        return Frame(data)
 
-        return Frame()
-
-    def Get_InfoMesage():
-        pass
+    def Get_InfoMesage( self, frame ):
+        mLen    = frame.dataFrame[0]
+        mId_dst = frame.dataFrame[1]
+        mId_src = frame.dataFrame[2]
+        mCommand= frame.dataFrame[3]
+        mPayload= frame.dataFrame[4]
