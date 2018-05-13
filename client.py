890952f66a048f53a08a01a2fd8d2050e499ff9f
alarcon <miguel.alarcon.ortiz@gmail.com>
2018-05-13 20:24:26 +0200

    implemented socket method whit class sockets

--- client.py
+++ client.py
@@ -2,14 +2,26 @@ import socket
 
 class Client ( object ):
     """docstring for ."""
+    _sock = None
     def __init__(self, arg):
+        global _sock
         self.arg = arg
-
-    def Connect(self):
-        pass
+        #creating socket TCP/IP.
+        _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    def Connect( self, server_address, server_port ):
+        #conection to the server.
+        server_data = (server_address, server_port)
+        _sock.connect(server_data)
+        
     def Disconnect(self):
-        pass
-    def SendInfo(self):
-        pass
+        #close the link with server.
+        _sock.close()
+        
+    def SendInfo(self, frame):
+        # this methos will take the class frame and 
+        _sock.sendall(frame.ToString())
+        
     def RecieveInfo(self):
-        pass
+        _sock.recv()
+    
